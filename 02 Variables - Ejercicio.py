684e223763a4e9ebc250a168161de6da492ba8e6
IndiMaya <168603065+IndiMaya@users.noreply.github.com>
2024-04-30 19:03:08 -0500

    Update 02 Variables - Ejercicio.py

--- 02 Variables - Ejercicio.py
+++ 02 Variables - Ejercicio.py
@@ -1,12 +1,12 @@
 #Escribe tu nombre en la línea 3.
 #Guarda tu nombre en la variable miNombre.
-
+miNombre= ("Indira")
 #Escribe el nombre de tu mejor amigo en la línea 6.
-#Guarda ese nombre en la variable miMejorAmigo.
-
+#Guarda ese nombre en la variable 
+miMejorAmigo=(" Julio ")
 #Escribe el nombre del lugar en el que conociste a tu mejor amigo en la línea 9.
-#Guarda ese nombre en la variable lugarEnQueNosConocimos
-
+#Guarda ese nombre en la variable 
+lugarEnQueNosConocimos=(" Miraflores ")
 #Corre el código. Debería imprimir tu nombre, el de tu mejor amigo y el lugar en que se conocieron.
 print("Mi nombre es:")
 print(miNombre)
